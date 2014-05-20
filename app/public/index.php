4acc9f83c8541c03e68ac1c409ed72903c4f7fc1
Adrian Teh <adrianteh@hotmail.com>
2014-05-20 11:20:04 +1000

    addded bootstrap view

---
+++ app/public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hierarchy</title>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+
+<nav class="navbar navbar-inverse">
+	<div class="navbar-header">
+		<a class="navbar-brand" href="#">Hierarchy</a>
+	</div>
+	<ul class="nav navbar-nav">
+		<li><a href="about.php">About</a></li>
+	</ul>
+</nav>
+
+<h1>Demo</h1>
+
+<table class="table table-striped table-bordered">
+	<thead>
+	</thead>
+	<tbody>
+	</tbody>
+</table>
+
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// redirect to public landing page
+header( 'Location: app/public/index.php' ) ;
+?>
